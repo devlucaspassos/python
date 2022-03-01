a51928e12f6dee3b98de3a4803acb54aae012da8
Lucas Passos <contato.passos@protonmail.com>
2022-03-01 13:10:33 -0300

    Initial commit

---
+++ aula1/aula1.py
@@ -0,0 +1,21 @@
+#  COMENTÁRIOS EM PYTHON
+print('Linha 1')
+print('Linha 2')
+# Há a possibilidade de criar comentários em uma linha
+print('Linha 3')
+print('Linha 4')
+# Assim como é possível a inserção de comentários de duas ou mais linhas
+# Segunda linha de comentário
+# Terceira linha de comentário
+print('Linha 5')
+print('Linha 6')  # Quando o comentário foi mesma linha, é recomendado inserir dois espaços após a função.
+#print('Linha 7') ---> Quando eu quiser que um linha do código seja ignorada, é possível usar as cerquilha para tranformar o código em comentário.
+
+# Para comentário de linhas multiplas, utiliza-se as três aspas seguidas tanto para abertura como para fechamento.
+
+"""print('Linha 7')  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu dolor at sapien efficitur accumsan. """ 
+print('Linha 8')
+''' ---> também é possível usar aspas simples 
+print('Linha 9') ---> Exemplo de multilinhas de código sendo tranformadas em comentários.
+print('Linha 9')
+'''
---
+++ aula2/aula2.py
@@ -0,0 +1,16 @@
+# print(123456)  # Nesse exemplo, os argumentos 123456 serão exibidos na tela.
+# print('Sorvete')   # Nesse exemplo, o argumento "Sorvete" será exebida na tela. 
+# print('Lucas', 'Passos', 'Qualquer coisa')  # Os arguementos podem ser separados por vírgula, e serão exibidos na tela com espaços entre sí.
+#print('Lucas', 'Passos', sep='-')  # utilizando o parâmetro sep, você pode substituir o espaço padrão entre as strings pelo -.
+# print('Lucas', 'Passos', sep='-', end='')
+# print('João', 'e', 'Maria', sep='-', end='')
+
+#Exercício: Monte um cpf separando os números com pontos e traço.
+
+print('201', '702', '305', sep='.', end='-')
+print('11')
+
+
+
+
+
