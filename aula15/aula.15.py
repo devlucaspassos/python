7cc773923e78ea2f697a8dc040756e16f6387a26
nitroknight1 <contato.passos@protonmail.com>
2022-04-20 11:43:57 -0300

    Aulas 15 e 16

--- aula15/aula.15.py
+++ aula15/aula.15.py
@@ -5,10 +5,10 @@ Manipulando strings
 * Funções built-in len, abs, type, print, etc...
 Essas funções podem ser usadas diretamente em cada tipo.
 '''
-#Positivos  [012345678^] --> cada caractere de ums string tem um índice, ex: 't' tem índice 2.
-texto =     'Python s2'
-#Negativos -[987654321]
+#Postivos        [012345678]
+texto          = 'Python S2'
 
-#exemplo de acesso ao índice 2 da string atribuída a variável "texto" acima.
+print(texto[3])
 
-print(texto[-9]) #A letra "t" será exibida, pois o índice da mesma é 2.  
+nova_string = texto[0::2]
+print(nova_string)
