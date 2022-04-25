df3ec42b05e82bf4443df55c1902e602f341a7e2
nitroknight1 <contato.passos@protonmail.com>
2022-04-25 13:31:29 -0300

    Aula 17

---
+++ aula17/aula17.py
@@ -0,0 +1,14 @@
+'''
+while / else
+contadores 
+acumuladores
+'''
+contador = 1 
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print('Aplicação do comando condicional else.')
